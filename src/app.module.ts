d0c305d35ced692b8e5234dfd6174bff8ae0fdea
Phùng Hùng <phunghung29@gmail.com>
2022-03-16 10:49:03 +0700

    generate resource

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,11 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { \[cat\]Module } from './[cat/]/[cat/].module';
+import { CatModule } from './cat/cat.module';
 
 @Module({
-  imports: [],
+  imports: [\[cat\]Module, CatModule],
   controllers: [AppController],
   providers: [AppService],
 })
